de398b2c8cf15f6ec91f269a2107febdfa65da61
NeerajKrishnaS <neerajkrishnabs@gmail.com>
2023-11-14 04:36:35 +0530

    Add files for pip

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(
-     name='math_quiz',
+    name='math_quiz',
     version='1.0.0',
     description='A math quiz',
     author='Neeraj Krishna',
@@ -8,11 +8,11 @@ setup(
     packages=['math_quiz'],
     install_requires=[
     
-    ]
-# No external dependencies for this code
+                     ],
+
     classifiers=[
        'Development Status :: Game',
-       'Intended Audience: Science/Research',
+       'Intended Audience:: Science/Research',
        'License :: OSI Approved :: Apache-2.0 license',
-    ]
+    ],
 )    
