5fc44716a5a54c00faddc79d0d59984ce89ece0c
NeerajKrishnaS <neerajkrishnabs@gmail.com>
2023-11-14 01:11:58 +0530

    add files for pip installing

--- setup.py
+++ setup.py
@@ -12,4 +12,19 @@ setup(
             'math_quiz=math_quiz.math_quiz:main',
         ],
     },
+    url='https://github.com/NeerajKrishnaS/dsss_homework_2',
+    license='Apache License',  # Choose an appropriate license
+    author='NeerajKrishnaS',
+    author_email='neerajKrishnabs@gmail.com',
+    description='A simple math quiz game',
+    long_description='A Python package for a simple math quiz game.',
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License v2.0',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
+
