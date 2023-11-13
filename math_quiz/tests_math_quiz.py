0a27e9290d1a630a5f9003a7c37c796a46b6bfe9
NeerajKrishnaS <neerajkrishnabs@gmail.com>
2023-11-13 23:11:50 +0530

    add unit tests for the math_quiz

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,31 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
-
+from math_quiz import random_integer, random_operator, math_problem
 
 class TestMathGame(unittest.TestCase):
-
-    def test_function_A(self):
+    def test_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_operator(self):
+        result = random_operator()
+        self.assertIn(result, ['+', '-', '*'])
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_math_problem(self):
+        test_cases = [
+            (3, 2, '+', '3 + 2', 5),
+            (1, 5, '-', '1 - 5', -4),
+            (10, 2, '*','10 * 2', 20),
+            
+        ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        for number1, number2, operator, expected_problem, expected_answer in test_cases:
+            problem, answer = math_problem(number1, number2, operator)
+            self.assertEqual(problem, expected_problem)
+            self.assertEqual(answer, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
